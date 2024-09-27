cf068ebca89151e2d265d6b525fe9acc07dbf6d4
goncalo.amador <goncalo.amador@symbioticsgroup.com>
2024-09-27 11:41:38 +0100

    Replacing moment with date-fns

--- src/app/modules/calendar/calendar.component.ts
+++ src/app/modules/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { format, startOfMonth, endOfMonth, startOfWeek, addDays, isSameMonth, isToday, addMonths } from 'date-fns';
 import { MatDialog } from '@angular/material/dialog';
 import { EditEventFormDialogComponent } from '../../modules/calendar/edit-event-form-dialog/edit-event-form-dialog.component';
 import { Event } from '../../shared/models/event.model';
@@ -14,16 +14,16 @@ import { AuthService } from 'src/app/core/services/auth.service';
 })
 export class CalendarComponent implements OnInit {
   sidebarVisible = true; // Default to true, will adjust based on screen size
-  currentMonthYear: moment.Moment = moment();
-  calendar: moment.Moment[][] = [];
+  currentMonthYear: Date = new Date();
+  calendar: Date[][] = [];
   events: { [key: string]: Event[] } = {}; // Store events by date
-
   isAdminRoute: boolean = false;
 
   constructor(
     public dialog: MatDialog,
     private route: ActivatedRoute,
-    private authService: AuthService) {}
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.adjustSidebarVisibility();
@@ -41,11 +41,11 @@ export class CalendarComponent implements OnInit {
   }
 
   adjustSidebarVisibility() {
-    this.sidebarVisible = window.innerWidth > 768; // Adjust the breakpoint as needed
+    this.sidebarVisible = window.innerWidth > 768;
   }
 
   getPublicHolidays(year: number): string[] {
-    const publicHolidays = [
+    return [
       `${year}-01-01`,
       `${year}-04-25`,
       `${year}-05-01`,
@@ -57,21 +57,20 @@ export class CalendarComponent implements OnInit {
       `${year}-12-08`,
       `${year}-12-25`
     ];
-    return publicHolidays;
   }
 
   generateCalendar(): void {
-    const startOfMonth = moment(this.currentMonthYear).startOf('month');
-    const endOfMonth = moment(this.currentMonthYear).endOf('month');
+    const startOfCurrentMonth = startOfMonth(this.currentMonthYear);
+    const endOfCurrentMonth = endOfMonth(this.currentMonthYear);
 
-    let calendarRows: moment.Moment[][] = [];
-    let day = moment(startOfMonth).startOf('week');
+    let calendarRows: Date[][] = [];
+    let day = startOfWeek(startOfCurrentMonth);
 
-    while (day.isBefore(endOfMonth)) {
-      let week: moment.Moment[] = [];
+    while (day <= endOfCurrentMonth) {
+      let week: Date[] = [];
       for (let i = 0; i < 7; i++) {
-        week.push(moment(day));
-        day.add(1, 'day');
+        week.push(day);
+        day = addDays(day, 1);
       }
       calendarRows.push(week);
     }
@@ -80,27 +79,27 @@ export class CalendarComponent implements OnInit {
   }
 
   moveMonth(offset: number): void {
-    this.currentMonthYear.add(offset, 'months');
+    this.currentMonthYear = addMonths(this.currentMonthYear, offset);
     this.generateCalendar();
   }
 
-  getDayClasses(day: moment.Moment): string[] {
-    const isCurrentMonthYear = day.isSame(this.currentMonthYear, 'month');
-    const isToday = day.isSame(moment(), 'day') && day.isSame(this.currentMonthYear, 'month');
-    const hasEvents = this.events[day.format('YYYY-MM-DD')] && this.events[day.format('YYYY-MM-DD')].length > 0;
+  getDayClasses(day: Date): string[] {
+    const isCurrentMonth = isSameMonth(day, this.currentMonthYear);
+    const today = isToday(day);
+    const hasEvents = this.events[format(day, 'yyyy-MM-dd')] && this.events[format(day, 'yyyy-MM-dd')].length > 0;
 
     let classes: string[] = [];
-    if (!isCurrentMonthYear) {
+    if (!isCurrentMonth) {
       classes.push('inactive');
-    } else if (isToday) {
+    }
+    if (today) {
       classes.push('today');
     }
-
     if (hasEvents) {
       classes.push('has-events');
     }
 
-    const isHoliday = this.getPublicHolidays(this.currentMonthYear.year()).includes(day.format('YYYY-MM-DD'));
+    const isHoliday = this.getPublicHolidays(this.currentMonthYear.getFullYear()).includes(format(day, 'yyyy-MM-dd'));
     if (isHoliday) {
       classes.push('holiday');
     }
@@ -108,8 +107,8 @@ export class CalendarComponent implements OnInit {
     return classes;
   }
 
-  selectDate(day: moment.Moment): void {
-    const dateStr = day.format('YYYY-MM-DD');
+  selectDate(day: Date): void {
+    const dateStr = format(day, 'yyyy-MM-dd');
     const eventsForDay = this.events[dateStr] || [];
 
     if (this.isAdminRoute) {
@@ -140,7 +139,6 @@ export class CalendarComponent implements OnInit {
     }
   }
 
-
   addEvent(eventData: { title: string, date: string | null, description: string }): void {
     if (eventData.title && eventData.date) {
       const newEvent: Event = {
@@ -152,7 +150,7 @@ export class CalendarComponent implements OnInit {
       if (!this.events[eventData.date]) {
         this.events[eventData.date] = [];
       }
-      this.events[eventData.date] = [...this.events[eventData.date], newEvent]; // Create new array to trigger change detection
+      this.events[eventData.date].push(newEvent); // Create new array to trigger change detection
       this.saveEvents();
       this.generateCalendar(); // Update calendar view
     }
@@ -181,7 +179,7 @@ export class CalendarComponent implements OnInit {
         delete this.events[date]; // Remove the date key if no events remain
       }
 
-      this.saveEvents(); // Save changes to localStorage
+      this.saveEvents(); // Save changes
       this.generateCalendar(); // Update calendar view
     }
   }
