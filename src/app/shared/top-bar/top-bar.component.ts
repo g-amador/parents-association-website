02233b9293d42d1d6af078bad398fecf2add3146
goncalo.amador <goncalo.amador@symbioticsgroup.com>
2024-12-08 00:55:05 +0000

    First top-bar version almost completely functional

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -7,11 +7,17 @@ import { AuthService } from '../../core/services/auth.service';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent {
-  selectedLink: string | null = null;
+  selectedLink: string | null = 'home'; // Default to 'home'
 
-  constructor(
-    private authService: AuthService)
-    {}
+  constructor(private authService: AuthService) { }
+
+  setSelectedLink(link: string): void {
+    console.log(link)
+    this.selectedLink = link;
+    // Optionally, trigger change detection if needed
+    // e.g., using ChangeDetectorRef or Angular's change detection system
+    // this.changeDetectorRef.detectChanges();  // Uncomment if using ChangeDetectorRef
+  }
 
   logout() {
     this.authService.logout();
