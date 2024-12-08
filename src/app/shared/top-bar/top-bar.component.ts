6e84e0e30d090eb14fb3da57b86efd8501664827
goncalo.amador <goncalo.amador@symbioticsgroup.com>
2024-12-08 00:16:12 +0000

    Adding first version of topbar with all options

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent {
+  selectedLink: string | null = null;
 
+  constructor(
+    private authService: AuthService)
+    {}
+
+  logout() {
+    this.authService.logout();
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
 }
